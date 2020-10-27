a29e8225ba8da409b1af61c05804f447086a854c
casalgado <casalgado86@gmail.com>
2020-10-27 09:32:52 -0500

    transition animation iframe

---
+++ src/components/EmbedFrame.vue
@@ -0,0 +1,166 @@
+<template>
+  <transition name="fade">
+    <div id="iframeCont" v-if="showFrame">
+      <iframe
+        :style="'height: ' + iframeHeight + 'px;'"
+        frameborder="no"
+        scrolling="no"
+        seamless
+        :src="src"
+        :key="frameKey"
+      ></iframe>
+      <p @click="hideFrame">
+        ×
+      </p>
+      <div class="dummies">
+        <div id="cover">
+          <div class="loading-gradient"></div>
+        </div>
+        <div id="titles"><div class="loading-gradient"></div></div>
+        <div id="progress"><div class="loading-gradient"></div></div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+import { gsap } from "gsap";
+export default {
+  name: "EmbedFrame",
+  data() {
+    return {
+      src: "",
+      iframeHeight: 170,
+      showFrame: false,
+    };
+  },
+  methods: {
+    hideFrame: function() {
+      this.showFrame = false;
+      this.$store.commit("setMediaUrl", "");
+    },
+  },
+  computed: {
+    mediaUrl() {
+      return this.$store.state.mediaUrl;
+    },
+  },
+  watch: {
+    mediaUrl() {
+      console.log("here");
+      this.src = this.mediaUrl;
+      if (this.src !== "") {
+        this.showFrame = true;
+        this.frameKey++;
+        setTimeout(function() {
+          gsap.to(".loading-gradient", {
+            duration: 0.01,
+            left: "-100%",
+            ease: "easeOut",
+          });
+          gsap
+            .to(".loading-gradient", {
+              duration: 1,
+              left: "-10%",
+              ease: "easeOut",
+            })
+            .delay(0.02);
+        });
+      }
+      if (this.src.includes("spotify")) {
+        this.iframeHeight = 150;
+      } else {
+        this.iframeHeight = 170;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+iframe {
+  position: relative;
+  bottom: 0px;
+  width: 100%;
+  z-index: 9;
+}
+
+#iframeCont {
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  z-index: 9;
+  line-height: 0px;
+  background-color: rgba(0, 0, 0, 0.9);
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+}
+
+.dummies * {
+  position: absolute;
+  float: left;
+  border-radius: 3px;
+  margin: 5px;
+  z-index: 8;
+  overflow: hidden;
+}
+
+.loading-gradient {
+  margin: 0px;
+  width: 200%;
+  position: relative;
+  left: -100%;
+  height: 100%;
+  background: rgb(32, 35, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(32, 35, 36, 0.9) 0%,
+    rgba(90, 90, 97, 0.9) 62%,
+    rgba(32, 35, 36, 0.9) 100%
+  );
+}
+
+#cover {
+  top: 0;
+  left: 0;
+  margin: 10px;
+  width: 130px;
+  height: 130px;
+  border-radius: 8px;
+}
+
+#titles {
+  bottom: 55px;
+  left: 150px;
+  width: 30%;
+  height: 30px;
+}
+
+#progress {
+  bottom: 20px;
+  left: 150px;
+  width: 80%;
+  height: 20px;
+}
+
+#iframeCont p {
+  position: absolute;
+  top: -12px;
+  right: 5px;
+  z-index: 10;
+  cursor: pointer;
+  line-height: 0px;
+  font-size: 25px;
+  width: 15px;
+  height: 15px;
+  color: var(--primary-accent);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,19 +15,7 @@
     <Wall />
     <Banner />
     <Contact />
-
-    <div id="iframeCont" v-if="showFrame">
-      <iframe
-        :style="'height: ' + iframeHeight + 'px;'"
-        frameborder="no"
-        scrolling="no"
-        seamless
-        :src="src"
-        :key="frameKey"
-      ></iframe>
-      <p @click="showFrame = false">×</p>
-    </div>
-
+    <EmbedFrame />
     <Footer />
   </div>
 </template>
@@ -40,45 +28,32 @@ import Banner from "@/components/Banner";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import IntroText from "@/components/IntroText";
+import EmbedFrame from "@/components/EmbedFrame";
 export default {
   name: "Home",
-  components: { Navbar, Landing, Wall, Banner, Contact, Footer, IntroText },
+  components: {
+    Navbar,
+    Landing,
+    Wall,
+    Banner,
+    Contact,
+    Footer,
+    IntroText,
+    EmbedFrame,
+  },
   data() {
-    return {
-      src: "",
-      iframeHeight: 170,
-      showFrame: false,
-    };
+    return {};
   },
   methods: {
     handleScroll() {
       this.$store.commit("setScroll", window.pageYOffset);
     },
-    testState() {
-      console.log(this.$store.state.mediaUrl);
-    },
   },
-  computed: {
-    mediaUrl() {
-      return this.$store.state.mediaUrl;
-    },
-  },
+
   created() {
     window.addEventListener("scroll", this.handleScroll);
   },
-  watch: {
-    mediaUrl() {
-      console.log("here");
-      this.src = this.mediaUrl;
-      this.showFrame = true;
-      this.frameKey++;
-      if (this.src.includes("spotify")) {
-        this.iframeHeight = 150;
-      } else {
-        this.iframeHeight = 170;
-      }
-    },
-  },
+
   mounted() {
     window.scrollTo(0, 0);
   },
@@ -114,36 +89,5 @@ export default {
   height: 150vh;
   background-color: var(--neutral-light);
   position: absolute;
-}
-
-iframe {
-  position: relative;
-  bottom: 0px;
-  width: 100%;
-  z-index: 9;
-}
-
-#iframeCont {
-  position: fixed;
-  bottom: 0px;
-  width: 100%;
-  z-index: 9;
-  line-height: 0px;
-  background-color: rgba(82, 45, 91, 0.8);
-  border-top-right-radius: 15px;
-  border-top-left-radius: 15px;
-}
-
-#iframeCont p {
-  position: absolute;
-  top: -12px;
-  right: 5px;
-  z-index: 10;
-  cursor: pointer;
-  line-height: 0px;
-  font-size: 25px;
-  width: 15px;
-  height: 15px;
-  color: var(--primary-accent);
 }
 </style>
