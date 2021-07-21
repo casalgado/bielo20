d9c9fdbe1f411276188dde7c6bec8f5e94348006
casalgado <casalgado86@gmail.com>
2021-07-21 18:06:32 -0500

    testing payu integration 2

--- src/components/Marketplace.vue
+++ src/components/Marketplace.vue
@@ -40,8 +40,73 @@ export default {
     },
   },
   methods: {
+    testPayUfetch: function () {
+      let init = {
+        method: "POST",
+      };
+      fetch(
+        "https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/",
+        init
+      ).then((response) => {
+        this.testApi = response;
+        console.log("response");
+        console.log(response);
+      });
+    },
     testPayU: function () {
       let data = {
+        merchantId: 508029,
+        ApiKey: "4Vj8eK4rloUd272L48hsrarnUA",
+        referenceCode: "TestPayU",
+        accountId: "512326",
+        description: "Test PAYU",
+        amount: 3,
+        tax: 0,
+        taxReturnBase: 0,
+        currency: "USD",
+        signature: "ba9ffa71559580175585e45ce70b6c37",
+        test: 1,
+        buyerEmail: "test@test.com",
+      };
+      console.log("test");
+      this.axios({
+        method: "post",
+        url: "https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/",
+        data: data,
+        transformRequest: [
+          function (data, headers) {
+            console.log(data);
+            console.log(headers);
+            return data;
+          },
+        ],
+      }).then((response) => {
+        this.testApi = response;
+        console.log("response");
+        console.log(response);
+      });
+    },
+  },
+  mounted() {
+    /* 
+    let data = {
+        merchantId: "508029",
+        accountId: "512321",
+        description: "Test PAYU",
+        referenceCode: "TestPayU",
+        amount: "20000",
+        tax: "3193",
+        taxReturnBase: "16806",
+        currency: "COP",
+        signature: "7ee7cf808ce6a39b17481c54f2c57acc",
+        test: "0",
+        buyerFullName: "testo test",
+        buyerEmail: "test@test.com",
+        responseUrl: "http://www.test.com/response",
+        confirmationUrl: "http://www.test.com/confirmation",
+      };
+
+    let data = {
         merchantId: 508029,
         ApiKey: "4Vj8eK4rloUd272L48hsrarnUA",
         referenceCode: "TestPayU",
@@ -55,25 +120,7 @@ export default {
         test: 1,
         buyerEmail: "test@test.com",
       };
-      let headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-      };
-
-      console.log("test");
-      this.axios
-        .post(
-          "https://sandbox.checkout.payulatam.com/ppp-web-gateway-payu/",
-          data,
-          headers
-        )
-        .then((response) => {
-          this.testApi = response;
-        });
-    },
-  },
-  mounted() {
-    this.axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+    */
   },
 };
 </script>
